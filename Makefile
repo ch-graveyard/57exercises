6d1c1f0f202222cd257a3a300c693fe1a64ffea5
Chua Hou <extremelyspicystudios@gmail.com>
2019-07-12 22:43:20 +0800

    Add Makefile and add automatic addition of make rules to initialisation shell script

---
+++ Makefile
@@ -0,0 +1,6 @@
+CFLAGS=-g -Wall -O3
+LDLIBS=
+CC=c99
+
+clean:
+	rm -rf */build/*
--- initproblem.sh
+++ initproblem.sh
@@ -11,6 +11,13 @@
 
 # Create directory
 mkdir $1$2
+mkdir $1$2/build
 
 # Create empty source file
 echo \/\* Problem $1: $3 \*\/ > $1$2/$2.c
+
+# Add build rule to makefile
+echo "" >> Makefile
+echo $1$2: $1$2/build/$2 >> Makefile
+echo $1$2/build/$2: $1$2/$2.c >> Makefile
+echo "	\$(CC) \$(CFLAGS) \$< -o \$@" >> Makefile
